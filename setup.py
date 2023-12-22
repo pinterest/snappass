ae79b6b5ae17e727cb45b846d0160bf850665d9f
Yuru Shao <yshao@pinterest.com>
2023-12-22 21:03:10 +0000

    Bump version: 1.6.1 → 1.6.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='snappass',
-    version='1.6.1',
+    version='1.6.2',
     description="It's like SnapChat... for Passwords.",
     long_description=(open('README.rst').read() + '\n\n' +
                       open('AUTHORS.rst').read()),
