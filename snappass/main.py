3f100ee6dff50bd01f991f16827c63c328337aba
Egg <45681670+Radical-Egg@users.noreply.github.com>
2024-04-17 16:42:55 -0700

    Update main.py
    Prefix bind address and port with SNAPPASS_

--- snappass/main.py
+++ snappass/main.py
@@ -353,8 +353,8 @@ def health_check():
 
 @check_redis_alive
 def main():
-    app.run(host=os.environ.get('BIND_ADDRESS', '0.0.0.0'),
-            port=os.environ.get('PORT', 5000))
+    app.run(host=os.environ.get('SNAPPASS_BIND_ADDRESS', '0.0.0.0'),
+            port=os.environ.get('SNAPPASS_PORT', 5000))
 
 
 if __name__ == '__main__':
